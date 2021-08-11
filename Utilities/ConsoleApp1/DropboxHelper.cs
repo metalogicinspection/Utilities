16434f59e2dbcc6126d807635ce5bf6db063193e
jiahuimetalogic <79609130+jiahuimetalogic@users.noreply.github.com>
2021-08-11 11:22:20 -0600

    added upload file to blob

---
+++ Utilities/ConsoleApp1/DropboxHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using DropboxUploader.Core;
+
+namespace ConsoleApp1
+{
+    internal class DropboxHelper
+    {
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://www.dropbox.com"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal static DropboxDirectClient GetDirectClient()
+        {
+            return new DropboxDirectClient(GetLocalDirBase(), _dropboxKey);
+
+        }
+
+        private static readonly string _dropboxKey = "TeATRlb9VWgAAAAAAAAAQlsO5puMgPEBYzp0zo16I8ii5kTwsHsKDlg4eNVVymjK";
+        //jiahui@hotmail.com "GYCluLX9W_AAAAAAAAAAXZqNZZcR2X8kBlqsACvIGwXcVOREH_eKhJcs0PK-MkZO";
+
+        internal static string GetLocalDirBase()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Metalogic",
+                FileUploadHelper.GenerateGuidStr(string.Concat("rAo!b8a4", _dropboxKey, "4a&8(")));
+        }
+
+        internal static string GetLocalUvDataPendingUploadFolder()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Metalogic",
+               "PendingUVDataUpload");
+        }
+
+
+
+    }
+}
---
+++ Utilities/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = DropboxHelper.GetDirectClient();
+            //client.UploadOnline(@"E:\Reporting\Radiographic\Radiographic\bin\Release\Radiographic.exe",
+            //    "/Radiographic.exe");
+
+            client.UploadOnline(
+                "C:\\Users\\User\\Documents\\dummy.uvdata",
+                "/dummy.uvdata");
+        }
+    }
+}
